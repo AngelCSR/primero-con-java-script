53694710d4a4d0fb10baa11c57603c7f6c22cd33
Angel <angelcamposanchezrey@gmail.com>
2025-02-03 20:22:26 +0100

    v3

---
+++ funciones.js
@@ -0,0 +1,67 @@
+ 
+        function verificaEdad(){
+            const mayoriaEdad =18; /** para declarar una constante que no va a cambiar**/
+            let edad = prompt('Dime tu edad');
+            if (edad<mayoriaEdad){
+                 document.write ("<h2>No puedes entrar a este sitio</h2>");
+                 }  else {
+                document.write("<h2>Bienvenido</h2>")
+                }
+            }
+           function mediaDosNumeros(a,b){
+               let suma=a+b;
+               return  suma/2;
+           }
+           function calculaMedia(){
+            let n1= prompt("Dime el primer numero")
+            let n2= prompt("Dime el segundo numero")
+            if (isNaN(n1)|| isNaN(n2)){
+                alert("Soo se aceptan numeros")
+            } else{
+            let media= mediaDosNumeros(Number(n1),Number(n2));
+            alert("La media de"+n1 +" y "+n2 +" es "+media)
+           }
+        }   
+    
+        function tablaMultiplicar(){
+            let numero=prompt("Dime el numero que quieres la tabla");
+            let cadena='';
+            for(let i=0;i<=10;i++){
+                cadena += numero+"*"+i+"= "+numero*i+"<br>"
+            }
+            document.write(cadena)
+        }
+        let alineación =['Raúl',' Benzemá',' Butragueño','Figo'];
+    
+        function posicion(){
+            let numero = prompt("Dime el  número")
+            if(numero>alineación.length || numero<1){
+                alert('Posición no ocupada')
+               
+            }else {
+            alert('En la posición '+numero+"juega "+alineación[numero-1]);
+            }
+        }
+        function imprimeAlineacion(){
+            let cadena='';
+            for(let i=0;i<alineación.length;i++){
+                cadena += i+" "+alineación[i]+"<br>";
+               
+    
+    
+            }
+            document.write(cadena);
+        }
+        function claculaAreaTriangulo(b,a){
+            return (b*a)/2
+        }
+
+
+        
+        function AreaTriangulo(){
+            let base =prompt("Dime el lado del triangulo")
+            let altura =prompt("Dime la altura del triangulo")
+            let media =claculaAreaTriangulo(base,altura)
+            alert(" El area del triangulo es: "+area)
+        }
+       
